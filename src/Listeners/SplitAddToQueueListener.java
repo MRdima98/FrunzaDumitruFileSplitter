f68480bc81b07f7e5cbe9f0046afb7379db1f332
MRdima98 <62344480+MRdima98@users.noreply.github.com>
2020-06-08 14:45:13 +0200

    Prova

--- src/Listeners/SplitAddToQueueListener.java
+++ src/Listeners/SplitAddToQueueListener.java
@@ -10,7 +10,11 @@ public class SplitAddToQueueListener implements ActionListener {
     FinalGui2_JTable T=new FinalGui2_JTable();
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("Add to queue"))
+        if(e.getActionCommand().equals("Add to queue")){
             T.AddRow();
+            System.out.println("Prova");
+        }
+
     }
+
 }
